9f404fcffc9f07600e6bb37f6d3c9754ab71e0d3
oncomouse <oncomouse@gmail.com>
2019-07-01 17:05:57 -0500

    Match HTML characters as errors, too.

--- main.go
+++ main.go
@@ -1,38 +1,38 @@
 package main
 
 import (
-  "net/http"
-  "io/ioutil"
-  "os"
-  "os/exec"
-  "fmt"
-  "regexp"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/exec"
+	"regexp"
 )
 
-func run_wego() {
-  out, err := exec.Command("wego").Output()
-  if err != nil {
-    fmt.Printf("%v", err)
-    os.Exit(1)
-  }
+func runWego() {
+	out, err := exec.Command("wego").Output()
+	if err != nil {
+		fmt.Printf("%v", err)
+		os.Exit(1)
+	}
 
-  fmt.Printf("%s", out)
+	fmt.Printf("%s", out)
 }
 
 func main() {
-  response, err := http.Get("http://wttr.in?format=2")
-  if err != nil {
-    run_wego()
-  }
-  defer response.Body.Close()
-  contents, err := ioutil.ReadAll(response.Body)
-  if err != nil {
-    run_wego()
-  }
-  matched, _ := regexp.Match("^[A-Za-z0-9]", contents)
-  if matched {
-    run_wego()
-  } else {
-    fmt.Printf("%s", contents)
-  }
+	response, err := http.Get("http://wttr.in?format=2")
+	if err != nil {
+		runWego()
+	}
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		runWego()
+	}
+	matched, _ := regexp.Match("^[<>A-Za-z0-9]", contents)
+	if matched {
+		runWego()
+	} else {
+		fmt.Printf("%s", contents)
+	}
 }
